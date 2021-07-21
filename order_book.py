ab7d0bd1d795ec53d79cb7e3a943da79d80e781e
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-07-21 12:00:47 +0800

    v2

--- order_book.py
+++ order_book.py
@@ -11,11 +11,11 @@ session = DBSession()
 def get_all_match_orders(order):
     
     cur_rate=order.buy_amount / order.sell_amount
-    res=session.query(Order).filter(Order.filled==None,Order.buy_currency==order.sell_currency,
+    select=session.query(Order).filter(Order.filled==None,Order.buy_currency==order.sell_currency,
                                 Order.sell_currency==order.buy_currency).all()
     result=[]
-    if len(res)>0:
-        for obj in res:
+    if len(select)>0:
+        for obj in select:
             #maker
             tmp_rate=obj.sell_amount / obj.buy_amount
             if tmp_rate>=cur_rate:
@@ -55,25 +55,25 @@ def insert_order(order):
 
 def process_order(order):
     #Your code here
-    obj=insert_order(order)
+    target=insert_order(order)
     existing_order=None
-    if obj!=None:
-        result=get_all_match_orders(obj)
+    if target!=None:
+        result=get_all_match_orders(target)
         if len(result)>0:
             sorted(result, key=lambda o: o.sell_amount, reverse=True)
             existing_order=result[0]
             #Set the filled field to be the current timestamp on both orders
             current_time=datetime.now()
             existing_order.filled=current_time
-            obj.filled=current_time
+            target.filled=current_time
             #Set counterparty_id to be the id of the other order
-            obj.counterparty_id=existing_order.id
-            existing_order.counterparty_id=obj.id
+            target.counterparty_id=existing_order.id
+            existing_order.counterparty_id=target.id
             #Create a new order for remaining balance
             new_order = None
-            if existing_order.buy_amount>obj.sell_amount:
+            if existing_order.buy_amount>target.sell_amount:
                 new_order=Order()
-                differ=existing_order.buy_amount - obj.sell_amount
+                differ=existing_order.buy_amount - target.sell_amount
                 new_order.buy_amount=differ
                 sell_amount=differ*existing_order.sell_amount/existing_order.buy_amount
                 new_order.sell_amount=sell_amount
@@ -82,17 +82,17 @@ def process_order(order):
                 new_order.buy_currency=existing_order.buy_currency
                 new_order.receiver_pk=existing_order.receiver_pk
                 new_order.sender_pk=existing_order.sender_pk
-            if  existing_order.buy_amount<obj.sell_amount:
+            if  existing_order.buy_amount<target.sell_amount:
                 new_order = Order()
-                differ = obj.sell_amount - existing_order.buy_amount
+                differ = target.sell_amount - existing_order.buy_amount
                 new_order.sell_amount = differ
-                buy_amount = differ * obj.buy_amount / obj.sell_amount
+                buy_amount = differ * target.buy_amount / target.sell_amount
                 new_order.buy_amount = buy_amount
-                new_order.creator_id = obj.id
-                new_order.sell_currency = obj.sell_currency
-                new_order.buy_currency = obj.buy_currency
-                new_order.receiver_pk = obj.receiver_pk
-                new_order.sender_pk = obj.sender_pk
+                new_order.creator_id = target.id
+                new_order.sell_currency = target.sell_currency
+                new_order.buy_currency = target.buy_currency
+                new_order.receiver_pk = target.receiver_pk
+                new_order.sender_pk = target.sender_pk
             if new_order!=None:
                 session.add(new_order)
             session.commit()
